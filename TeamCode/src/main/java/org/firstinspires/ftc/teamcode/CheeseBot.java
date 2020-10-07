ea88a9afd476f1977960f65eb9e44d32de798ce8
Radmin <klannen+5899@libertycommon.org>
2020-10-06 20:59:42 -0600

    got basic cheese boy working when using teleop mode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CheeseBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CheeseBot.java
@@ -5,67 +5,115 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 @TeleOp(name="Cheesy Boi", group="Test")
-@Disabled
+//@Disabled
 public class CheeseBot  extends LinearOpMode {
+        //All hardware
+        protected DcMotor motorFwdLeft = null;
+        protected DcMotor motorFwdRight = null;
+        protected DcMotor motorBackLeft = null;
+        protected DcMotor motorBackRight = null;
 
-    // Declare OpMode members.
-    private ElapsedTime runtime = new ElapsedTime();
-    /*
-    private DcMotor leftDrive = null;
-    private DcMotor rightDrive = null;
-    */
-    @Override
-    public void runOpMode() {
-        telemetry.addData("Status", "Initialized");
-        telemetry.update();
-        /*
-        // Initialize the hardware variables. Note that the strings used here as parameters
-        // to 'get' must correspond to the names assigned during the robot configuration
-        // step (using the FTC Robot Controller app on the phone).
-        leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
-        rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
+        //Variables
+        private double max = 1.0;
+        double maxPower;
+        double powerLim = 1;
+        double moveDir = -1;
 
-        // Most robots need the motor on one side to be reversed to drive forward
-        // Reverse the motor that runs backwards when connected directly to the battery
-        leftDrive.setDirection(DcMotor.Direction.FORWARD);
-        rightDrive.setDirection(DcMotor.Direction.REVERSE);
-        */
-        // Wait for the game to start (driver presses PLAY)
-        waitForStart();
-        runtime.reset();
+        @Override
+        public void runOpMode() {
+            //Prepares all the hardware
+            motorFwdRight = hardwareMap.get(DcMotor.class, "motorFwdRight");
+            motorBackLeft = hardwareMap.get(DcMotor.class, "motorBackLeft");
+            motorFwdLeft = hardwareMap.get(DcMotor.class, "motorFwdLeft");
+            motorBackRight = hardwareMap.get(DcMotor.class, "motorBackRight");
+            motorFwdLeft.setDirection(DcMotor.Direction.REVERSE);
+            motorBackLeft.setDirection(DcMotor.Direction.REVERSE);
 
-        // run until the end of the match (driver presses STOP)
-        while (opModeIsActive()) {
-            /*
-            // Setup a variable for each drive wheel to save power level for telemetry
-            double leftPower;
-            double rightPower;
-
-            // Choose to drive using either Tank Mode, or POV Mode
-            // Comment out the method that's not used.  The default below is POV.
-
-            // POV Mode uses left stick to go forward, and right stick to turn.
-            // - This uses basic math to combine motions and is easier to drive straight.
-            double drive = -gamepad1.left_stick_y;
-            double turn  =  gamepad1.right_stick_x;
-            leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
-            rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
-
-            // Tank Mode uses one stick to control each wheel.
-            // - This requires no math, but it is hard to drive forward slowly and keep straight.
-            // leftPower  = -gamepad1.left_stick_y ;
-            // rightPower = -gamepad1.right_stick_y ;
-
-            // Send calculated power to wheels
-            leftDrive.setPower(leftPower);
-            rightDrive.setPower(rightPower);
-            */
-            // Show the elapsed game time and wheel power.
-            telemetry.addData("Status", "Run Time: " + runtime.toString());
-            telemetry.addData("Motors", "left (%.2f), right (%.2f)",1 , 1);
+            telemetry.addData("Status", "Initialized");
             telemetry.update();
+
+            //variables
+            boolean changed3 = false;
+            boolean changed4 = false;
+
+            // Wait for the game to start (driver presses PLAY)
+            waitForStart();
+            // run until the end of the match (driver presses STOP)
+            while (opModeIsActive()) {
+                //collects input from the joysticks
+                double fwdBackPower = this.gamepad1.left_stick_y * moveDir;
+                double strafePower = this.gamepad1.left_stick_x * moveDir;
+                double turnPower = this.gamepad1.right_stick_x;
+
+                //does math to figure the power that should be applied to every motor
+                double leftFrontPower = (fwdBackPower - turnPower - strafePower)*powerLim;
+                double rightFrontPower = (fwdBackPower + turnPower + strafePower)*powerLim;
+                double leftBackPower = (fwdBackPower - turnPower + strafePower)*powerLim;
+                double rightBackPower = (fwdBackPower + turnPower - strafePower)*powerLim;
+
+
+                maxPower = Math.abs(leftFrontPower);
+                if (Math.abs(rightFrontPower) > maxPower) {
+                    maxPower = Math.abs(rightFrontPower);
+                }
+                if (Math.abs(leftBackPower) > maxPower) {
+                    maxPower = Math.abs(leftBackPower);
+                }
+                if (Math.abs(rightBackPower) > maxPower) {
+                    maxPower = Math.abs(rightBackPower);
+                }
+                if (maxPower > 1) {
+                    leftFrontPower = leftFrontPower / maxPower;
+                    rightFrontPower = rightFrontPower / maxPower;
+                    leftBackPower = leftBackPower / maxPower;
+                    rightBackPower = rightBackPower / maxPower;
+                    telemetry.addData("Works",true);
+                    telemetry.update();
+                }
+                //sets the power of the motors
+                motorFwdLeft.setPower(leftFrontPower*max);
+                motorFwdRight.setPower(rightFrontPower*max);
+                motorBackLeft.setPower(leftBackPower*max);
+                motorBackRight.setPower(rightBackPower*max);
+
+
+                //toggle for speed and direction of the bot for easier control
+                if(gamepad1.b && !changed3) {//speed limiter toggle
+                    if(powerLim == .5){
+                        powerLim = 1;
+                    }
+                    else{
+                        powerLim = .5;
+                    }
+                    changed3 = true;
+                } else if(!gamepad1.b){changed3 = false;}
+
+                if(gamepad1.a && !changed4) {//direction change toggle
+                    if(moveDir == 1){
+                        moveDir = -1;
+                    }
+                    else{
+                        moveDir = 1;
+                    }
+                    changed4 = true;
+                } else if(!gamepad1.a){changed4 = false;}
+                telemetry.addData("Wheel Position", motorFwdLeft.getCurrentPosition()); //to be used when the encoders are ready
+                telemetry.addData("Max Speed",powerLim);
+                telemetry.addData("Direction",moveDir);
+                telemetry.update();
+            }
         }
     }
-}
