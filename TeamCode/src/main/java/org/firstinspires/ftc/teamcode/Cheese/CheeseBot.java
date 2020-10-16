736b5821361f26ec5222f590dc434365fffc55d0
bosch.richard24 <bosch.richard24@libertycommon.org>
2020-10-15 20:12:11 -0600

    TEST

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Cheese/CheeseBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Cheese/CheeseBot.java
@@ -44,7 +44,7 @@ public class CheeseBot extends LinearOpMode {
             telemetry.addData("Status", "Initialized");
             telemetry.update();
 
-            //variables
+            //variables - TEST
             boolean changed3 = false;
             boolean changed4 = false;
 
