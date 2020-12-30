018e4c05fce4f02c00d744c75b2783f67e0bf8bd
Radmin <klannen+5899@libertycommon.org>
2020-12-30 10:46:48 -0700

    added odometry programs. Need to switch the orientation of some of the variables

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestPrograms/Odometry/GlobalCoordinatePositionUpdateSample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestPrograms/Odometry/GlobalCoordinatePositionUpdateSample.java
@@ -21,7 +21,8 @@ public class GlobalCoordinatePositionUpdateSample extends LinearOpMode {
     final double COUNTS_PER_INCH = 307.699557;
 
     //Hardware map names for the encoder wheels. Again, these will change for each robot and need to be updated below
-    String verticalLeftEncoderName = "rf", verticalRightEncoderName = "lf", horizontalEncoderName = "lb";
+    String rfName = "motorFwdRight", rbName = "motorBackRight", lfName = "motorFwdLeft", lbName = "motorBackLeft";
+    String verticalLeftEncoderName = rbName, verticalRightEncoderName = lfName, horizontalEncoderName = rfName;
 
     @Override
     public void runOpMode() throws InterruptedException {
